be6dcbadff2062b2851b07da8919990e85e461df
DEINFRA\vitorzapellini <victorzapellini@sie.sc.gov.br>
2024-11-07 17:03:12 -0300

    Adicionado efeito de carrosel | Adicionado section de 'sobre nós'

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-var teste = 'asdasd'
+// Funcionalide do carrossel
+
+let currentGroupIndex = 0;
+
+function nextImages() {
+  const groups = document.querySelectorAll('.grupo-imagens');
+  groups[currentGroupIndex].classList.remove('active');
+
+  currentGroupIndex = (currentGroupIndex + 1) % groups.length;
+
+  groups[currentGroupIndex].classList.add('active');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.grupo-imagens').classList.add('active');
+});
