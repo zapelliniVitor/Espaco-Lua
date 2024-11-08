1d910a0058fc901117bcbe5d097746a2655fdefd
DEINFRA\vitorzapellini <victorzapellini@sie.sc.gov.br>
2024-11-08 19:00:32 -0300

    Seção de avaliações adicionadas | Função para trocar a avaliação apresentada por tempo ou clique

--- script.js
+++ script.js
@@ -30,14 +30,18 @@ document.querySelectorAll(".accordion-button").forEach((button) => {
   });
 });
 
-// Formulário de Consulta
-
+// Envio de E-mail
 document
   .getElementById("marqueConsultaForm")
   .addEventListener("submit", function (event) {
     event.preventDefault();
 
     const formData = new FormData(this);
+
+    for (let [key, value] of formData.entries()) {
+      console.log(key, value);
+    }
+
     formData.append("service_id", "service_uzn43eo");
     formData.append("template_id", "template_kunf0ql");
     formData.append("user_id", "VQp7Ykno-i_7s8CvC");
@@ -59,5 +63,33 @@ document
       .catch((error) => {
         alert("Erro ao enviar o e-mail: " + error.message);
       });
-    event.preventDefault();
   });
+
+// Avaliações
+
+let currentIndex = 0;
+const items = document.querySelectorAll(".avaliacao-item");
+const pontos = document.querySelectorAll(".avaliacao-ponto");
+
+function mostrarAvaliacao(index) {
+  items.forEach((item, i) => {
+    item.classList.remove("active");
+    pontos[i].classList.remove("ativo");
+    if (i === index) {
+      item.classList.add("active");
+      pontos[i].classList.add("ativo");
+    }
+  });
+}
+
+function iniciarTrocaAutomatica() {
+  setInterval(() => {
+    currentIndex = (currentIndex + 1) % items.length;
+    mostrarAvaliacao(currentIndex);
+  }, 5000); // Intervalo de 5 segundos
+}
+
+window.onload = () => {
+  mostrarAvaliacao(currentIndex);
+  iniciarTrocaAutomatica();
+};
